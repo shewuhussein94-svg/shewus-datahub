df8b4386e2b258d81b12a790335cae6d2b4b8801
Shewu Hussein <shewuhussein94@gmail.com>
2026-05-05 03:46:53 +0000

    fix: remove duplicate PORT declaration

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('*', (req, res) => {
 });
 
 // app.listen MUST BE LAST
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
